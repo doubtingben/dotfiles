ba781f23ea9c6dac6563a29741dc2cf24e9cb685
Ben Wilson <doubtingben@gmail.com>
2016-10-28 09:38:26 -0400

    added emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,25 @@
+
+(menu-bar-mode -1)
+(show-paren-mode 1)
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("fe230d2861a13bb969b5cdf45df1396385250cc0b7933b8ab9a2f9339b455f5c" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-theme 'monokai t)
