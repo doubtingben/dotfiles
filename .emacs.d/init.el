f3bf0f09f2c5798818f98552a2c0a4643c9d8325
Ben Wilson <doubtingben@gmail.com>
2016-11-09 11:56:58 -0500

    remove emacs junk

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -49,3 +49,12 @@
     (send-string-to-terminal (concat "\033]2; " (buffer-name) "\007"))))
 
   (add-hook 'post-command-hook 'xterm-title-update)
+
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+    version-control t)       ; use versioned backups
